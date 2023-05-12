a4efb7d796af773dba3c2cad20a20f9d47d56918
Denis Denisov <umdom1@gmail.com>
2023-05-12 22:03:38 +0300

    second task

--- main.py
+++ main.py
@@ -21,6 +21,28 @@ with open('recipes.txt', 'rt') as file:
     cook_book [dish_name] = composition
   pprint(cook_book, sort_dicts=False)
 
+
+# second task
+
+def get_shop_list_by_dishes(dishes, person_count):
+  indigrients_count = {}
+  indigrient = {}
+
+  for el in dishes:
+    if el in cook_book.keys():
+      for li in cook_book[el]:
+        indigrient = {('measure': li['measure']),
+                      indigrient['quantity'] = int(li['quantity']) * person_count
+        print(indigrient)
+        if li['ingredient_name'] in indigrients_count.keys():
+          indigrient['quantity'] = indigrient['quantity'] + (int(li['quantity']) * person_count)
+        else:
+          indigrients_count[li['ingredient_name']] = indigrient
+
+        print(indigrients_count)
+
+        dishes = ['Омлет', 'Фахитос']
+        get_shop_list_by_dishes(dishes, 10)
   
 
 
